0b13a6b83722b861b057dde3c1a257aef10d1682
Jonathon Grigg <jonathongrigg@gmail.com>
2017-11-28 10:11:26 +1100

    Update sound sensor example to use a lower threshold

--- examples/soundsensor-example/fs/init.js
+++ examples/soundsensor-example/fs/init.js
@@ -3,8 +3,10 @@ load('api_tektboard.js');
 
 // normally you'd have an average of readings or maybe hard code a
 // predetermined value but I'm lazy and this still shows the main idea
-// add a bit extra so it only turns on when we shine a light on it
-let threshold = Grove.SoundSensor.getRaw(TektBoard.ANALOG_GROVE_PIN) + 200;
+// add a bit extra so it only turns on for noticable sounds
+// note that these sensors are a bit troublesome to work with as far as finding
+// a threshold that suits your use case
+let threshold = Grove.SoundSensor.getRaw(TektBoard.ANALOG_GROVE_PIN) + 100;
 
 Grove.SoundSensor.attach(TektBoard.ANALOG_GROVE_PIN, threshold, 100 /* 10 Hz */
         function(avgValue) {
